ba19b65b6a77cf7cb1ce4d78ccc0d735da74b9de
roel-vereycken <vereyckenroel@gmail.com>
2021-07-14 20:44:31 +0200

    test done

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Contact() {
+  const [message, setMessage] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };
   return (
     <div className="contact" id="contact">
-      Hallo
+      <div className="left">
+        <h2>Prentje</h2>
+      </div>
+      <div className="right">
+        <h2>Contact</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+          {message && <span>I will reply asap</span>}
+        </form>
+      </div>
     </div>
   );
 }
